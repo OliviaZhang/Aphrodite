5fc06bdae301898ac4b43f3c9b402b0270c10abe
OliviaZhang <sealife24@gmail.com>
2016-02-27 15:11:01 -0500

    use ui.router instead of ngRoute, more flexible

--- public/javascripts/appAngular.js
+++ public/javascripts/appAngular.js
@@ -1,18 +1,50 @@
 var app = angular.module('Aphrodite', ['ui.router']);
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('register', {
-            url: '/register',
-            templateUrl: 'views/index.ejs',
-            controller: 'RegisterController'
-        });
+
+app.config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+
+
+      $stateProvider
+          .state('home',{
+                url: '/',
+                controller: 'HomeCtrl',
+                templateUrl: '/views/home.ejs'
+          })
+          .state('register',{
+                url: '/register',
+                templateUrl: '/views/register.ejs',
+                controller: 'RegisterCtrl'
+          })
+          .state('login',{
+                url: '/login',
+                templateUrl: '/views/login.ejs',
+                controller: 'LoginCtrl'
+          })
+          .state('inside',{
+                url: '/inside',
+                templateUrl: '/views/inside.ejs',
+                controller: 'InsideCtrl'
+          });
 
     $urlRouterProvider.otherwise('home');
+
+
 }]);
 app.controller('MainCtrl', ['$scope', function($scope) {
     $scope.test = 'Hello world';
 }]);
 
-app.controller('RegisterController', ['$scope', function($scope) {
-    $scope.test = 'register';
+app.controller('HomeCtrl', ['$scope', function($scope) {
+    $scope.test = 'Hello Home!!';
+}]);
+
+app.controller('RegisterCtrl', ['$scope', function($scope) {
+    $scope.test = 'Hello Register!!';
+}]);
+
+app.controller('LoginCtrl', ['$scope', function($scope) {
+    $scope.test = 'Hello Login!!';
+}]);
+
+app.controller('InsideCtrl', ['$scope', function($scope) {
+    $scope.test = 'Hello Member!!';
 }]);
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var config = require('./config/database');
 var User = require('./app/models/user');
 var port = process.env.PORT || 8080;
 var jwt = require('jwt-simple');
+var routes = require('./routes');
 
 // connect to database
 mongoose.connect(config.database);
@@ -100,9 +101,13 @@ app.use(morgan('dev'));
 
 app.use(passport.initialize());
 app.set('view engine', 'ejs');
-
-app.use('/api', apiRoutes);
-
+app.set('views',__dirname + '/public/views'); //set absolute path for views folder 
 app.use(express.static(__dirname + '/public'));
+app.use('/api', apiRoutes);
+app.get('/', function(req, res) {
+    res.render('index')
+});
+
+
 app.listen(port);
 console.log('Magic happens!');
